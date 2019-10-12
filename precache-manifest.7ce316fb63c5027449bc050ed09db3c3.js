ab6cad2c39caa1d6da5dc247ece946f34225f382
MrRav3n <dawidmrraven@gmail.com>
2019-10-12 13:17:40 +0200

    Updates

---
+++ precache-manifest.7ce316fb63c5027449bc050ed09db3c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f24ce2c7b4459337921",
+    "url": "github.com/MrRav3n/Basic-token-ERC20.git/static/js/runtime~main.8f24ce2c.js"
+  },
+  {
+    "revision": "11c5f936452cbe0b75cc",
+    "url": "github.com/MrRav3n/Basic-token-ERC20.git/static/js/main.11c5f936.chunk.js"
+  },
+  {
+    "revision": "9e07386cf5c5d0888431",
+    "url": "github.com/MrRav3n/Basic-token-ERC20.git/static/js/1.9e07386c.chunk.js"
+  },
+  {
+    "revision": "11c5f936452cbe0b75cc",
+    "url": "github.com/MrRav3n/Basic-token-ERC20.git/static/css/main.7d0ef67c.chunk.css"
+  },
+  {
+    "revision": "9e07386cf5c5d0888431",
+    "url": "github.com/MrRav3n/Basic-token-ERC20.git/static/css/1.8bb04c1f.chunk.css"
+  },
+  {
+    "revision": "205dbd57ba21269e388beb411bebf017",
+    "url": "github.com/MrRav3n/Basic-token-ERC20.git/index.html"
+  }
+];
