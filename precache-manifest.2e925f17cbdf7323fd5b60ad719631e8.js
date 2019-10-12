7788923f839c0142f4abba96322d0c90ddc3c9f7
MrRav3n <dawidmrraven@gmail.com>
2019-10-12 15:57:53 +0200

    Updates

---
+++ precache-manifest.2e925f17cbdf7323fd5b60ad719631e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb6624a9f22e2d5b2b76",
+    "url": "/Basic-token-ERC20/static/js/runtime~main.bb6624a9.js"
+  },
+  {
+    "revision": "2cab1475eb59485b1775",
+    "url": "/Basic-token-ERC20/static/js/main.2cab1475.chunk.js"
+  },
+  {
+    "revision": "7df8f5ba373cf1731226",
+    "url": "/Basic-token-ERC20/static/js/1.7df8f5ba.chunk.js"
+  },
+  {
+    "revision": "2cab1475eb59485b1775",
+    "url": "/Basic-token-ERC20/static/css/main.0cd6b5bc.chunk.css"
+  },
+  {
+    "revision": "7df8f5ba373cf1731226",
+    "url": "/Basic-token-ERC20/static/css/1.1052d772.chunk.css"
+  },
+  {
+    "revision": "77492b6de8a3aed352013766eee1087d",
+    "url": "/Basic-token-ERC20/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "github.com/MrRav3n/Basic-token-ERC20/precache-manifest.7d68d347559465050ce211dcc4265f77.js"
+  "/Basic-token-ERC20/precache-manifest.2e925f17cbdf7323fd5b60ad719631e8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("github.com/MrRav3n/Basic-token-ERC20/index.html", {
+workbox.routing.registerNavigationRoute("/Basic-token-ERC20/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
